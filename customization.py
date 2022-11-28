4ec1dc410a6d2fbf897355a034937b510723ac24
ahmed <hamdiahmed906@gmail.com>
2022-11-28 10:51:51 +0200

    control customization finished

---
+++ customization.py
@@ -0,0 +1,43 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+
+class Customization(QMainWindow):
+    #control_signal = pyqtSignal(list)
+
+    def __init__(self, pager : QStackedWidget):
+        super(Customization,self).__init__()
+        uic.loadUi("customization.ui",baseinstance=self, resource_suffix='_rc')
+        self.pager = pager
+
+        self.back_button = self.findChild(QToolButton, "bt_back")
+        self.back_button.clicked.connect(lambda: self.pager.setCurrentIndex(self.pager.currentIndex()-1))
+        
+        self.findChild(QPushButton, "bt_save").clicked.connect(self.saveControls)
+        self.feed_label = self.findChild(QLabel, "lb_feed")
+
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("back.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.back_button.setIcon(icon)
+
+    def saveControls(self):
+        controls = []
+        controls.append(self.findChild(QComboBox, "combo_up").currentText())
+        controls.append(self.findChild(QComboBox, "combo_down").currentText())
+        controls.append(self.findChild(QComboBox, "combo_left").currentText())
+        controls.append(self.findChild(QComboBox, "combo_right").currentText())
+        controls.append(self.findChild(QComboBox, "combo_fire").currentText())
+        
+        if(self.validateControls(controls)):
+            self.feed_label.setText("Saved Sucessfully !")
+        else:
+            self.feed_label.setText("Invalid. Controls must be unique.")
+
+    def validateControls(self, l : list)->bool:
+        if len(l) == len(set(l)):
+            return True
+        else:
+            return False
+    
--- launcher.py
+++ launcher.py
@@ -4,21 +4,24 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
 from functools import partial
+from customization import Customization
 
 class Launcher(QMainWindow):
-	def __init__(self ):
+	def __init__(self, pager : QStackedWidget):
 		super(Launcher,self).__init__()
 		uic.loadUi("launcher.ui",baseinstance=self, resource_suffix='_rc')
-		self.show()
-		self.setFixedSize(800,600)
+		self.pager = pager
 		self.storyButton = self.findChild(QPushButton, "bt_story")
 		self.storyButton.clicked.connect(partial(self.handleStartButton, "story"))
 		self.findChild(QPushButton, "bt_endless").clicked.connect(partial(self.handleStartButton, "endless"))
 		self.findChild(QPushButton, "bt_vs").clicked.connect(partial(self.handleStartButton, "vs"))
+		
+
 
 	def handleStartButton(self, type : str):
 		if type == "vs":
 			#start vs mode
+			self.pager.setCurrentIndex(self.pager.currentIndex()+1)
 			print("vs")
 		elif type == "endless":
 			#start endless mode
@@ -26,10 +29,17 @@ class Launcher(QMainWindow):
 		else:
 			#start story mode
 			print("story")
-
 		
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    ui = Launcher()
+    widget = QtWidgets.QStackedWidget()
+    ui = Launcher(widget)
+    c = Customization(widget)
+    #c.control_signal.connect(ui.getControls)
+    widget.addWidget(ui)
+    widget.addWidget(c)
+    widget.setFixedSize(800,600)
+    widget.show()
+    
     app.exec_()
