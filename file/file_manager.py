922897b97699e6f2e1b46486d23e476d505b22f2
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2022-12-01 01:53:05 +0200

    Update file_manager.py

--- file/file_manager.py
+++ file/file_manager.py
@@ -4,30 +4,39 @@ import filepath
 import os
 import json
 import pygame
-from cryptography.fernet import Fernet
-
+from cryptography.fernet import Fernet, InvalidToken
+from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWUSR
 
 class FileManager:
     def __init__(self):
         pass
 
-    __p_dir = os.path.join(filepath.ROOT_DIR, "projects")
+    __p_dir = os.path.join(filepath.ROOT_DIR, "profiles")
     __fernet = Fernet(b'lBW8DUQf7LkNAuDhTGeBCPHfytQ6dJP7TjVTM5_KlwE=')
 
     def create_profile(self, name):
         """
         Creates new profile
-        :param name: username
+        :param name: (str) username
         :return: object of class Profile if the profile is created successfully
-                 False otherwise
+        False otherwise
         """
         u_path = os.path.join(self.__p_dir, name + ".txt")
         profile = Profile()
         profile.set_name(name)
-        file = self.__create_new_file(name)
         if not os.path.exists(self.__p_dir):
             os.mkdir(self.__p_dir)
-        if self.__write_file(file, u_path):
+        if self.__write_file(profile.get_profile(), u_path):
+            p_path = os.path.join(self.__p_dir, "profiles.txt")
+            profiles = [name]
+            if os.path.exists(p_path):
+                    p = self.__read_file("profiles")
+                    if p is not None and p != "C":
+                        profiles = p["profiles"]
+                        profiles.append(name)
+            if profiles is None or not self.__write_file({"profiles": profiles}, p_path):
+                self.delete_profile(name)
+                return False
             return profile
         else:
             return False
@@ -35,9 +44,9 @@ class FileManager:
     def save_profile(self, profile: Profile):
         """
         Saves the profile progress
-        :param profile (Profile): a profile to save
+        :param profile:  Profile object to save
         :return: True if profile is saved successfully
-                 False otherwise
+        False otherwise
         """
         u_path = os.path.join(self.__p_dir, profile.get_name() + ".txt")
         if not os.path.exists(u_path):
@@ -59,11 +68,13 @@ class FileManager:
         Gets a list of current users
         :return: string list of the usernames
         """
-        if os.path.exists(self.__p_dir):
-            profile_list = os.listdir(self.__p_dir)
-            profile_list = [p.split('.')[0] for p in profile_list]
-            return profile_list
-        return False
+        p_path = os.path.join(self.__p_dir, "profiles" + ".txt")
+        if os.path.exists(p_path):
+            profiles = self.__read_file("profiles")
+            if profiles is None or profiles == "C":
+                return False
+            else:
+                return profiles["profiles"]
 
     def delete_profile(self, name):
         """
@@ -75,6 +86,13 @@ class FileManager:
         path = os.path.join(self.__p_dir, name + ".txt")
         if os.path.exists(path):
             try:
+                p_path = os.path.join(self.__p_dir, "profiles" + ".txt")
+                if os.path.exists(p_path):
+                    profiles = self.get_profiles()
+                    if profiles:
+                        profiles.remove(name)
+                    self.__write_file({"profiles": profiles}, p_path)
+                os.chmod(path, S_IWUSR | S_IREAD)
                 os.remove(path)
                 return True
             except OSError:
@@ -88,12 +106,8 @@ class FileManager:
         :return: True if profile is loaded successfully
                  False otherwise
         """
-        try:
-            with open(os.path.join(self.__p_dir, name + ".txt"), 'rb') as openfile:
-                file = self.__fernet.decrypt(openfile.read())
-                json_object = json.loads(file.decode())
-                print(json_object)
-        except OSError:
+        json_object = self.__read_file(name)
+        if json_object is None or json_object == "C":
             return False
         profile = Profile()
         profile.set_name(json_object["name"])
@@ -133,29 +147,26 @@ class FileManager:
         except OSError:
             return False
 
-    def __create_new_file(self, name):
-        """
-        helper function to create new profile
-        :param name(str): username
-        :return: dict of with profile attributes
-        """
-        return {
-            "name": name,
-            "achievements": [],
-            "controls": {"left": "", "right": "", "up": "", "down": "", "fire": ""},
-            "story_progress": 0,
-            "unlocked_weapons": [],
-            "current_weapon": "",
-            "skins": [],
-            "current_skin": ""
-        }
-
     def __write_file(self, file, path):
         try:
+            if os.path.exists(path):
+                os.chmod(path, S_IWUSR | S_IREAD)
             with open(path, "wb") as outfile:
                 json_object = json.dumps(file, indent=4)
                 encrypted_file = self.__fernet.encrypt(json_object.encode())
                 outfile.write(encrypted_file)
+                os.chmod(path, S_IREAD | S_IRGRP | S_IROTH)
                 return True
         except OSError:
             return False
+
+    def __read_file(self, filename):
+        try:
+            with open(os.path.join(self.__p_dir, filename + ".txt"), 'rb') as openfile:
+                file = self.__fernet.decrypt(openfile.read())
+                json_object = json.loads(file.decode())
+                return json_object
+        except (InvalidToken, TypeError):
+            return "C"
+        except OSError:
+            return None
