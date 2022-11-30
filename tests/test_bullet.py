a09f093516e36326ce7e07d3d6a1df7fe944afbc
aaserfawzy <87318695+aaserfawzy@users.noreply.github.com>
2022-11-30 12:09:23 +0200

    pytest2

--- tests/test_bullet.py
+++ tests/test_bullet.py
@@ -1,15 +1,16 @@
 import sys
 sys.path.insert(0, './Entities')
+sys.path.insert(0, './assets_handler')
 from bullet import bullet
-
+from spritesheet import SpriteSheet
 import pygame
 import os
 
-img= pygame.image.load(os.path.join("Assets","Enemies_26x26_[6,2].png"))
-
+bullet_sheet = pygame.image.load(os.path.join("assets", "Bullets_10x16_[4,2].png"))
+BULLET_SHIP_SKINS = SpriteSheet(bullet_sheet,10,16,1,2).skin
 def test_bullet():
 	#assumtion
-	temp=bullet(10,20,img,50,5,0,1)
+	temp=bullet(10,20,BULLET_SHIP_SKINS[0],50,5,0,1)
 	
 	# action
 	temp.move()
