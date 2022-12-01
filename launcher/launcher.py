813ef10afaf23f3312dbce5155421b4332a95937
a7med-hamdy <hamdiahmed906@gmail.com>
2022-12-01 14:05:09 +0200

    commented launcher code

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -10,7 +10,9 @@ from launcher.customization import Customization
 import os
 import sys
 
-
+'''
+A thread to run the game on
+'''
 class Game_Thread(QThread):
     deadSignal = pyqtSignal()
     
@@ -18,16 +20,21 @@ class Game_Thread(QThread):
         super().__init__(parent)
         self.controller = None
         
-    
+    '''
+    a function that sets the engine controller
+    paramter(e) : engineController instance
+    '''
     def setController(self, e : engineController)->None:
         self.controller = e		
   
     def run(self):
+        # thread's main function
         print("game thread running")
         self.controller.run()
         self.stop()
 
     def stop(self):
+        # stop the thread on finish
         print("thread killed")
         self.deadSignal.emit()
         self.quit()
@@ -39,16 +46,19 @@ class Game_Thread(QThread):
 class Launcher(QMainWindow):
 	def __init__(self, pager : QStackedWidget, manager: FileManager):
 		super(Launcher,self).__init__()
+		# intialize ui
 		uic.loadUi(os.path.join("launcher","ui","launcher.ui"),baseinstance=self, resource_suffix='_rc')
+		# pager to navigate between views
 		self.pager = pager
-		self.controller = None
-		self.game_thread = Game_Thread()
+		self.controller = None # controller
+		self.manager = manager # file manager
+		self.game_thread = Game_Thread() # game thread
 		self.game_thread.deadSignal.connect(lambda : sys.exit())
 		self.storyButton = self.findChild(QRadioButton, "bt_story")
 		self.endlessButton = self.findChild(QRadioButton, "bt_endless")
 		self.vsButton = self.findChild(QRadioButton, "bt_vs")
 		self.storyButton.setChecked(True)
-		self.manager = manager
+		
 		# customization button setup
 		self.customButton = self.findChild(QPushButton, "bt_custom")
 		self.customButton.clicked.connect(lambda : self.pager.setCurrentIndex(self.pager.currentIndex()+1))
@@ -56,6 +66,7 @@ class Launcher(QMainWindow):
 		#back button setup
 		self.back_button = self.findChild(QToolButton, "bt_back")
 		self.back_button.clicked.connect(lambda: self.pager.setCurrentIndex(self.pager.currentIndex()-1))
+		# back button icon
 		icon = QtGui.QIcon()
 		icon.addPixmap(QtGui.QPixmap(os.path.join("launcher","assets","back.png")), QtGui.QIcon.Normal, QtGui.QIcon.On)
 		self.back_button.setIcon(icon)
@@ -63,11 +74,15 @@ class Launcher(QMainWindow):
 		self.playButton = self.findChild(QPushButton, "bt_play")
 		self.playButton.clicked.connect(self.handlePlayButton)
 
-		self.controls = None
+		# the chosen profile
 		self.profile = None
 
+	'''
+	a function that handles the event of click on 'play' button
+ 	'''
 	def handlePlayButton(self):
 		self.pager.hide()
+		# load assets
 		assets, backgrounds = self.manager.load_assets()
 		
 		if self.storyButton.isChecked():
@@ -90,7 +105,6 @@ class Launcher(QMainWindow):
 		self.profile = 	s
 
 	def catchControls(self, c : dict):
-		self.controls = c
 		self.profile.set_controls(c)
 		self.manager.save_profile(self.profile)
 		
