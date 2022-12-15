9572636e349bb91376ea9fa94d0849c5a6cd7475
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2022-12-16 00:29:30 +0200

    DA-51 fixed serialization bugs

--- assets_handler/skin.py
+++ assets_handler/skin.py
@@ -29,3 +29,27 @@ class Skin():
             if self.currframe >= len(self.frames):
                 self.currframe = 0
         return self.frames[self.currframe]
+
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state["frames_string"] = self.convert_to_string_list(state.pop("frames"))
+        return state
+
+    def __setstate__(self, state):
+        state["frames"] = self.convert_to_surface_list(state.pop("frames_string"))
+        self.__dict__.update(state)
+
+    def convert_to_string_list(self, surface_list):
+        surface_string = []
+        for surface in surface_list:
+            s = (pygame.image.tostring(surface, "RGB"), surface.get_size())
+            surface_string.append(s)
+        return surface_string
+
+    def convert_to_surface_list(self, surface_strings):
+        surfaces = []
+        for surface in surface_strings:
+            s = pygame.image.fromstring(surface[0], surface[1], "RGB")
+            surfaces.append(s)
+        return surfaces
