4cef9f57fd41dbb72820658f4934edf58dc69f05
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2022-12-15 20:07:15 +0200

    DA-51 <save and autosave modified>

--- file/gamestatesaver.py
+++ file/gamestatesaver.py
@@ -10,20 +10,38 @@ class GameStateSaver:
     def __init__(self):
         pass
 
-    def save_story_game_state(self, user_name, game_state: gameState):
+    def save_story(self, user_name, game_state: gameState):
         game_state_path = os.path.join(self.__dir, user_name, "story_progress.pkl")
         return self.__write_pickle_file(game_state, game_state_path)
 
-    def save_endless_game_state(self, user_name, game_state: gameState):
-        game_state_path = os.path.join(self.__dir, user_name, "endless_progress.pkl")
-        return self.__write_pickle_file(game_state, game_state_path)
 
-    def autosave_game_state(self, user_name, game_state: gameState):
-        autosave_path = os.path.join(self.__dir, user_name, "autosave.pkl")
-        backup_path = os.path.join(self.__dir, user_name, "backup.pkl")
+    def autosave_endless(self, user_name, game_state: gameState):
+        autosave_path = os.path.join(self.__dir, user_name, "endless_autosave.pkl")
+        backup_path = os.path.join(self.__dir, user_name, "endless_backup.pkl")
+        self.__autosave_game_state(game_state, autosave_path, backup_path)
+
+    def autosave_story(self, user_name, game_state: gameState):
+        autosave_path = os.path.join(self.__dir, user_name, "story_autosave.pkl")
+        backup_path = os.path.join(self.__dir, user_name, "story_backup.pkl")
+        self.__autosave_game_state(game_state, autosave_path, backup_path)
+
+    def load_saved_story(self, user_name):
+        game_state_path = os.path.join(self.__dir, user_name, "story_progress.pkl")
+        return self.__read_pickle_file(game_state_path)
+
+    def load_autosaved_story(self, user_name):
+        autosave_path = os.path.join(self.__dir, user_name, "story_autosave.pkl")
+        backup_path = os.path.join(self.__dir, user_name, "story_backup.pkl")
+        return self.__load_autosaved_state(autosave_path, backup_path)
+
+    def load_autosaved_endless(self, user_name):
+        autosave_path = os.path.join(self.__dir, user_name, "endless_autosave.pkl")
+        backup_path = os.path.join(self.__dir, user_name, "endless_backup.pkl")
+        return self.__load_autosaved_state(autosave_path, backup_path)
+
+    def __autosave_game_state(self, game_state, autosave_path, backup_path):
         if not os.path.exists(autosave_path):
             self.__write_pickle_file(game_state, autosave_path)
-
         else:
             prev_state = self.__read_pickle_file(autosave_path)
             if prev_state is not None:
@@ -33,21 +51,11 @@ class GameStateSaver:
             else:
                 self.__write_pickle_file(game_state, autosave_path)
 
-    def load_saved_story_game_state(self, user_name):
-        saved_state_path = os.path.join(self.__dir, user_name, "story_progress.pkl")
-        return self.__read_pickle_file(saved_state_path)
-
-    def load_saved_endless_game_state(self, user_name):
-        saved_state_path = os.path.join(self.__dir, user_name, "endless_progress.pkl")
-        return self.__read_pickle_file(saved_state_path)
-
-    def load_autosaved_state(self, user_name):
-        autosave_path = os.path.join(self.__dir, user_name, "autosave.pkl")
+    def __load_autosaved_state(self, autosave_path, backup_path):
         autosave = self.__read_pickle_file(autosave_path)
         if autosave is not None:
             return autosave
         # autosave is corrupted or deleted check back up.
-        backup_path = os.path.join(self.__dir, user_name, "backup.pkl")
         return self.__read_pickle_file(backup_path)
 
     def __write_pickle_file(self, object, path):
@@ -68,3 +76,11 @@ class GameStateSaver:
 
 
 
+
+game = gameState
+
+saver = GameStateSaver()
+
+saver.save_story_game_state("soso", gameState)
+t = saver.load_saved_story("soso")
+print(t)
