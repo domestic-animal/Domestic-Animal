de84f67175c5284e33b3d6890989e2cff47990d6
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2022-12-25 01:12:24 +0200

    DA-77 spritesheet & factory modification

--- assets_handler/dynamicAssetsFactory.py
+++ assets_handler/dynamicAssetsFactory.py
@@ -6,11 +6,10 @@ class dynamicAssetsFactory:
     The factory class for the *animated* assets that contains frames
     """
 
-    def __init__(self, cooldown = 100):
+    def __init__(self):
         """
         Constructor: loads all the animated assets
         """
-        self.cooldown = cooldown
         # Loading the animated spritesheet images
         self.BOSSES_IMAGE = pygame.image.load("Assets\Bosses_138x192_[2,6].png")
         self.BULLETS_IMAGE = pygame.image.load("Assets\Bullets_10x16_[4,2].png")
@@ -21,28 +20,28 @@ class dynamicAssetsFactory:
 
     def create_skins(self, asset, scale = 1, cooldown = 100):
         """
-        This function creates the SpriteSheet object and returns its 'skin' array
-
+        This function creates the SpriteSheet object and
+        Returns its 'skin' array
         :param asset: String describing the assets wanted
         :param cooldown: Refresh rate for the animation in milliseconds
         """
         ss: SpriteSheet = None 
         if asset == "bosses":
-            ss = SpriteSheet(self.BOSSES_IMAGE, 138, 192, scale, 6, 2)
+            ss = SpriteSheet(self.BOSSES_IMAGE, 138, 192, scale, 6, 2, cooldown)
 
         elif asset == "bullets":
-            ss = SpriteSheet(self.BULLETS_IMAGE, 10, 16, scale, 2, 4)
+            ss = SpriteSheet(self.BULLETS_IMAGE, 10, 16, scale, 2, 4, cooldown)
 
         elif asset == "enemies":
-            ss = SpriteSheet(self.ENEMIES_IMAGE, 26, 26, scale, 2, 6)
+            ss = SpriteSheet(self.ENEMIES_IMAGE, 26, 26, scale, 2, 6, cooldown)
 
         elif asset == "enemies_bullets":
-            ss = SpriteSheet(self.ENEMIES_BULLETS_IMAGE, 15, 24, scale, 3, 4)
+            ss = SpriteSheet(self.ENEMIES_BULLETS_IMAGE, 15, 24, scale, 3, 4, cooldown)
 
         elif asset == "powerups":
-            ss = SpriteSheet(self.POWERUPS_IMAGE, 31, 31, scale, 2, 5)
+            ss = SpriteSheet(self.POWERUPS_IMAGE, 31, 31, scale, 2, 5, cooldown)
 
         elif asset == "ships":
-            ss = SpriteSheet(self.SHIPS_IMAGE, 16, 16, scale, 2, 8)
+            ss = SpriteSheet(self.SHIPS_IMAGE, 16, 16, scale, 2, 8, cooldown)
 
         return ss.skin
