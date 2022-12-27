c7c2956ada0dc1ee31c40afaf70c09ed77cd551c
aaserfawzy <87318695+aaserfawzy@users.noreply.github.com>
2022-12-27 19:29:46 +0200

    DA-76_tests_refactor factors_tests_refactor

--- tests/test_enemy_factory.py
+++ tests/test_enemy_factory.py
@@ -16,18 +16,44 @@ ENEMY_SKINS = SpriteSheet(enemy_sheet,26,26,1,2,6).skin
 bullet_sheet = pygame.image.load(os.path.join("Assets", "Bullets_10x16_[4,2].png"))
 BULLET_SHIP_SKINS = SpriteSheet(bullet_sheet,10,16,1,2).skin
 
-def test_right_instance():
+def test_right_instance_dog():
     
     factor=enemyFactory(ENEMY_SKINS,ENEMY_SKINS,ENEMY_SKINS)
     
     temp1=factor.create("d",1,50,50,(600,600))
+
+    assert isinstance(temp1, dog)
+
+
+def test_right_instance_cat():
+    
+    factor=enemyFactory(ENEMY_SKINS,ENEMY_SKINS,ENEMY_SKINS)
     
     temp2=factor.create("c",1,50,50,(600,600))
 
+    assert isinstance(temp2, cat)
+
+def test_right_instance_bossDog():
+    
+    factor=enemyFactory(ENEMY_SKINS,ENEMY_SKINS,ENEMY_SKINS)
+
     temp3=factor.create("bd",1,50,50,(600,600))
 
+    assert isinstance(temp3, bossDog)
+
+
+def test_right_instance_bossCat():
+    
+    factor=enemyFactory(ENEMY_SKINS,ENEMY_SKINS,ENEMY_SKINS)
+    
     temp4=factor.create("bc",1,50,50,(600,600))
+
+    assert isinstance(temp4, bossCat)
+
+def test_right_instance_wrong():
+    
+    factor=enemyFactory(ENEMY_SKINS,ENEMY_SKINS,ENEMY_SKINS)
 
     temp5=factor.create("lk",1,50,50,(600,600))
 
-    assert isinstance(temp1, dog) and isinstance(temp2, cat) and  isinstance(temp3, bossDog) and isinstance(temp4, bossCat) and temp5==None
+    assert temp5==None
