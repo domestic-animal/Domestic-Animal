2223ad47514c235031ac1e2fb446d8676831dc67
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2022-12-28 17:00:52 +0200

    fix issue 9

--- assets_handler/music.py
+++ assets_handler/music.py
@@ -1,7 +1,4 @@
-import os
-os.environ['SDL_AUDIODRIVER'] = 'dsp'
 from typing import MutableSequence
-from pygame import mixer
 import random
 import pygame
 
@@ -19,25 +16,25 @@ class Music():
 
     def loadTrack(self, track = 0):
         """
-        Loads the wanted track into the pygame.mixerer.music
+        Loads the wanted track into the pygame.pygame.mixerer.music
         
         :param track: the wanted track to play (-1 for random track)
         """
-        mixer.music.unload()
+        pygame.mixer.music.unload()
         self.track = track
         if track == -1: #random track
             self.track = random.randint(0, len(self.music) - 1)
-        mixer.music.load(self.music[self.track])
+        pygame.mixer.music.load(self.music[self.track])
 
     def play(self):
-        mixer.music.play(-1)
+        pygame.mixer.music.play(-1)
 
     def pauseToggle(self):
         if not self.paused:
-            mixer.music.pause()
+            pygame.mixer.music.pause()
         else:
-            mixer.music.unpause()
+            pygame.mixer.music.unpause()
         self.paused = not self.paused
     
     def stop(self):
-        mixer.music.stop()
+        pygame.mixer.music.stop()
